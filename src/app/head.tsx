ba350b8fa3e352292a7aca977306b4e84ba0e1f2
drower22 <ismar@pitzei.com.br>
2025-07-04 16:10:37 -0300

    fix: instalação definitiva do Google Tag Manager via next/script no App Router

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Dex | Seu Resumo Financeiro Inteligente no WhatsApp</title>
+      <meta name="description" content="Receba o resumo diário do seu restaurante (iFood) direto no WhatsApp. Saiba seu lucro, controle taxas e evite prejuízos. Teste grátis!" />
+
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/Header';
 import PageWrapper from '@/components/PageWrapper';
 import FloatingWhatsApp from '@/components/FloatingWhatsApp';
 import Footer from '@/components/Footer';
+import Script from 'next/script';
 
 const sora = Sora({
   subsets: ['latin'],
@@ -56,6 +57,18 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${sora.variable} ${inter.variable}`}>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZNB6LSC" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {/* Google Tag Manager Script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KZNB6LSC');"
+          }}
+        />
+        {/* End Google Tag Manager Script */}
         <Header />
         <PageWrapper>
           {children}
